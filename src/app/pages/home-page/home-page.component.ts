b21128d01635520e3b437f0003998509d287e0af
Adrien <adriensauvage59@gmail.com>
2024-02-08 20:26:38 +0100

    ajout boucle for pour automatiser les card + ajout bouton fixe

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RecipeCardComponent } from "../recipe-card/recipe-card.component";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.scss',
-    imports: [RecipeCardComponent]
+    imports: [RecipeCardComponent, MatIconModule]
 })
 export class HomePageComponent {
+  recipes = [1,1,1,1,1,1];
 
 }

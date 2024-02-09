600d48106ff52ab896e4e3a9122a081e473911cd
Adrien Sauvage <94073350+AdriSauv@users.noreply.github.com>
2024-02-09 11:15:03 +0100

    ajout du reste des methodes cRud pour service recipe

--- src/app/services/Recipe/recipe-service.service.ts
+++ src/app/services/Recipe/recipe-service.service.ts
@@ -42,4 +42,37 @@ export class RecipeServiceService {
             [newRecipe,...currentState.recipes]})
       })
   )}
+
+  updateRecipe(recipe:any):Observable<any>{
+    const headers = this.getHeaders();
+    return this.http.post(`${this.apiUrl}/api/recipe/{recipe.id}`,recipe,{headers}).pipe(
+      tap((updatedRecipe:any)=>{
+        const currentState = this.recipeSubject.value;
+        const updatedRecipes = currentState.recipes.map((item:any)=>item.id === updatedRecipe.id?updatedRecipe:item)
+        this.recipeSubject.next({...currentState,recipes:updatedRecipes})
+      })
+    )
+  }
+
+  deleteRecipe(id:string):Observable<any>{
+    const headers = this.getHeaders();
+    return this.http.delete(`${this.apiUrl}/api/recipe/${id}`,{headers}).pipe(
+      tap((deletedRecipe:any)=>{
+        const currentState = this.recipeSubject.value;
+        const updatedRecipes = currentState.recipes.filter((item:any)=>item.id !== deletedRecipe.id)
+        this.recipeSubject.next({...currentState,recipes:updatedRecipes})
+      })
+    )
+  }
+
+  likeRecipe(id:any):Observable<any>{
+    const headers = this.getHeaders();
+    return this.http.post(`${this.apiUrl}/api/recipe/${id}/like`,{}, { headers }).pipe(
+      tap((likedRecipe:any)=>{
+        const currentState = this.recipeSubject.value;
+        const updatedRecipes = currentState.recipes.map((item:any)=>item.id === likedRecipe.id?likedRecipe:item)
+        this.recipeSubject.next({...currentState,recipes:updatedRecipes})
+      })
+    )
+  }
 }

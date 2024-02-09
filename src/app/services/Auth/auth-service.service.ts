64a9d5deebc4097008e05b213413e49fa0b7bd00
Adrien Sauvage <94073350+AdriSauv@users.noreply.github.com>
2024-02-09 10:56:18 +0100

    mise en place des methode d'authentification

---
+++ src/app/services/Auth/auth-service.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  private apiUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  authSubject = new BehaviorSubject<any>({
+    user:null,
+  });
+
+  login(userData:any):Observable<any>{
+    return this.http.post(`${this.apiUrl}/auth/signin`,userData);
+  }
+
+  register(userData:any):Observable<any>{
+    return this.http.post(`${this.apiUrl}/auth/signup`,userData);
+  }
+
+  getUserProfile():Observable<any>{
+    const headers = new HttpHeaders({
+      Authorization:`Bearer ${localStorage.getItem('jwt')}`
+    })
+    return this.http.get(`${this.apiUrl}/auth/user/profile`,{headers}).pipe(
+      tap((user)=>{
+        const currentState = this.authSubject.value;
+        this.authSubject.next({...currentState,user})
+      
+      })
+    )
+  }
+
+  logout(){
+    localStorage.clear();
+    this.authSubject.next({})
+  }
+
+
+}
